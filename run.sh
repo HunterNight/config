7969c1637ab2c004fc9f70ef4fbf95044130c781
HunterNight <trungson.gl1996@gmail.com>
2021-08-03 16:04:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
+mkdir -p ./logs/$(date +%Y%m%d)
 git pull
-java -jar agent.jar -jnlpUrl http://touch.sonnt.dynu.net/computer/test/jenkins-agent.jnlp -secret 1874c1d87892155c4ec4c2a02fa78e8c7302ea1061052128b220b20c2df254f3 -workDir "/opt/os/config" &
