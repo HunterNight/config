b8e69d663cfa3b71d3e64ef1007c652b43eec36b
HunterNight <trungson.gl1996@gmail.com>
2021-08-03 15:37:28 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-java -jar agent.jar -jnlpUrl http://touch.sonnt.dynu.net/computer/test/jenkins-agent.jnlp -secret 479a41b14f898304603a7a66eaf3b2962f718b81e40933f266a82db35be17480 -workDir "/opt/os/config"
+java -jar agent.jar -jnlpUrl http://touch.sonnt.dynu.net/computer/test/jenkins-agent.jnlp -secret 479a41b14f898304603a7a66eaf3b2962f718b81e40933f266a82db35be17480 -workDir "/opt/os/config" &
